d6fd67be1188d835612260d773eb28011af56971
lionben89 <lionben89@gmail.com>
2021-12-14 23:27:10 +0200

    initial LSTM

--- classifier.py
+++ classifier.py
@@ -45,11 +45,12 @@ class LRClassifier(Classifier):
 
     def evaluate(self, X_test, Y_test):
         pass
-    
+
+
 class SVMClassifier(Classifier):
-    def __init__(self, kernel = 'liner', gamma = 'scale'):
+    def __init__(self, kernel='liner', gamma='scale'):
         super().__init__()
-        self.svm = SVC(kernel=kernel,gamma=gamma)
+        self.svm = SVC(kernel=kernel, gamma=gamma)
 
     def get_cls(self):
         return self.svm
@@ -66,10 +67,10 @@ class SVMClassifier(Classifier):
 
 
 class BasicNN(Classifier):
-    def __init__(self, shape, n_epochs=50, criterion=nn.CrossEntropyLoss()):  # X_train_s.shape[1]
+    def __init__(self, input_shape, n_epochs=50, criterion=nn.CrossEntropyLoss()):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Linear(shape, 64),
+            nn.Linear(input_shape, 64),
             nn.ReLU(),
             nn.Linear(64, 2),
             nn.Sigmoid()
@@ -87,8 +88,70 @@ class BasicNN(Classifier):
         y_train = torch.LongTensor(y_train)
 
         for i in range(self.n_epochs):
-            i += 1
-            y_pred = self.model.forward(X_train) ## no batchs ?
+            y_pred = self.model.forward(X_train)  # no batchs ?
+            loss = self.criterion(y_pred, y_train)
+            self.losses.append(loss)
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+        return None
+
+    def predict(self, X_test):
+        tensor_pred = self.model(torch.Tensor(X_test))
+        return tensor_pred.max(1).indices
+
+    def evaluate(self, X_test, Y_test):
+        pass
+
+
+class LSTMNET(nn.Module):
+    def __init__(self, input_shape ,n_layers, linear_dim, dropout=0.5):
+        super(LSTMNET, self).__init__()
+        self.n_layers = n_layers
+        self.linear_dim = linear_dim
+        
+        self.lstm = nn.LSTM(input_shape, linear_dim, n_layers, dropout=dropout, batch_first=True)
+        self.dropout = nn.Dropout(dropout)
+        self.fc = nn.Linear(linear_dim, 2)
+        self.sigmoid = nn.Sigmoid()
+        
+    def forward(self, x, y_tag):
+        batch_size = 1
+        x = x.long()
+        if (y_tag is None):
+            y_tag = torch.zeros(self.n_layers, batch_size, self.linear_dim)
+        lstm_out, y_tag = self.lstm(x, y_tag)
+        lstm_out = lstm_out.contiguous().view(-1, self.linear_dim)
+        
+        out = self.dropout(lstm_out)
+        out = self.fc(out)
+        out = self.sigmoid(out)
+        
+        out = out.view(batch_size, -1)
+        out = out[:,-1]
+        return out, y_tag  
+
+
+class LSTMClassifier(Classifier):
+    def __init__(self,  input_shape ,n_layers, linear_dim, dropout=0.5, n_epochs=50, criterion=nn.CrossEntropyLoss()):
+        super().__init__()
+        self.model = LSTMNET(input_shape ,n_layers, linear_dim, dropout)
+        self.criterion = criterion
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
+        self.n_epochs = n_epochs
+        self.losses = []
+
+    def get_cls(self):
+        return self.model
+
+    def train(self, X_train, y_train):
+        X_train = torch.Tensor(X_train)
+        y_train = torch.LongTensor(y_train)
+        y_tag = None
+        for i in range(self.n_epochs):
+            y_pred, y_tag = self.model.forward(X_train,y_tag)  # no batchs ?
             loss = self.criterion(y_pred, y_train)
             self.losses.append(loss)
 
--- driver.py
+++ driver.py
@@ -3,7 +3,7 @@ from sklearn.linear_model import LogisticRegression
 from preprocess import preprocess
 from vectorize import TFIDF, W2VGensim, W2VReduced, W2VGlove, MeanW2V, ConcatW2V
 from kfold import KFoldCV
-from classifier import LRClassifier, BasicNN, SVMClassifier
+from classifier import LRClassifier, BasicNN, SVMClassifier, LSTMClassifier
 
 if __name__ == '__main__':
     vectore_size = 50
@@ -14,11 +14,12 @@ if __name__ == '__main__':
     # vectorize = ConcatW2V(W2VGlove(),vectore_size,number_of_words)
     vectorize = ConcatW2V(W2VReduced('./gensim_reduced.pkl'),vectore_size,number_of_words)
     
-    cls = BasicNN(shape=vectore_size*number_of_words)
+    # cls = BasicNN(shape=vectore_size*number_of_words)
     # cls = LRClassifier()
     # cls = SVMClassifier(kernel='linear')
     # cls = SVMClassifier(kernel='rbf')
     # cls = SVMClassifier(kernel='poly')
+    cls = LSTMClassifier(vectore_size,12,64,dropout=0.2)
     
     kf = KFoldCV(n_splits=5, shuffle=True)
     ds = preprocess("trump_train.tsv")
