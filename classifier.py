c98fd408d783e62a1c697858c3e6030959d4d55f
lionben89 <lionben89@gmail.com>
2021-12-15 15:07:57 +0200

    LSTM working

--- classifier.py
+++ classifier.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
 import numpy as np
+from sklearn.utils import shuffle
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 import torch
 import torch.nn as nn
+from metrics import evaluate_metrics
 
 
 class Classifier(ABC):
@@ -67,10 +69,10 @@ class SVMClassifier(Classifier):
 
 
 class BasicNN(Classifier):
-    def __init__(self, input_shape, n_epochs=50, criterion=nn.CrossEntropyLoss()):
+    def __init__(self, input_size, n_epochs=50, criterion=nn.CrossEntropyLoss()):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Linear(input_shape, 64),
+            nn.Linear(input_size, 64),
             nn.ReLU(),
             nn.Linear(64, 2),
             nn.Sigmoid()
@@ -95,6 +97,9 @@ class BasicNN(Classifier):
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
+            with torch.no_grad():  
+                y_pred = self.model.forward(X_train)
+                print("epoch: {}. loss:{}. metrics:{}".format(i,self.losses[-1],evaluate_metrics(y_train,y_pred.max(1).indices)))
 
         return None
 
@@ -107,37 +112,45 @@ class BasicNN(Classifier):
 
 
 class LSTMNET(nn.Module):
-    def __init__(self, input_shape ,n_layers, linear_dim, dropout=0.5):
+    def __init__(self, input_size ,n_layers, linear_dim, dropout=0.5):
         super(LSTMNET, self).__init__()
         self.n_layers = n_layers
         self.linear_dim = linear_dim
-        
-        self.lstm = nn.LSTM(input_shape, linear_dim, n_layers, dropout=dropout, batch_first=True)
+        self.input_size = input_size
+        self.lstm = nn.LSTM(input_size, linear_dim, n_layers, dropout=dropout, batch_first=True)
         self.dropout = nn.Dropout(dropout)
         self.fc = nn.Linear(linear_dim, 2)
         self.sigmoid = nn.Sigmoid()
         
-    def forward(self, x, y_tag):
-        batch_size = 1
-        x = x.long()
-        if (y_tag is None):
-            y_tag = torch.zeros(self.n_layers, batch_size, self.linear_dim)
-        lstm_out, y_tag = self.lstm(x, y_tag)
-        lstm_out = lstm_out.contiguous().view(-1, self.linear_dim)
+    def forward(self, x):
+        batch_size = x.size(0)
+        # Initializing hidden state for first input with zeros
+        h0 = torch.zeros(self.n_layers, batch_size, self.linear_dim).requires_grad_()
+
+        # Initializing cell state for first input with zeros
+        c0 = torch.zeros(self.n_layers, batch_size, self.linear_dim).requires_grad_()
+
+        # We need to detach as we are doing truncated backpropagation through time (BPTT)
+        # If we don't, we'll backprop all the way to the start even after going through another batch
+        # Forward propagation by passing in the input, hidden state, and cell state into the model
+        lstm_out, (hn, cn) = self.lstm(x, (h0, c0))
+
+        lstm_out =  lstm_out[:, -1, :]
         
         out = self.dropout(lstm_out)
         out = self.fc(out)
         out = self.sigmoid(out)
         
-        out = out.view(batch_size, -1)
-        out = out[:,-1]
-        return out, y_tag  
+        # out = out.view(batch_size, -1)
+        # out = out[:,-1]
+        return out  
 
 
 class LSTMClassifier(Classifier):
-    def __init__(self,  input_shape ,n_layers, linear_dim, dropout=0.5, n_epochs=50, criterion=nn.CrossEntropyLoss()):
+    def __init__(self,  input_size ,n_layers, linear_dim, dropout=0.5, n_epochs=50, batch_size=32, criterion=nn.CrossEntropyLoss()):
         super().__init__()
-        self.model = LSTMNET(input_shape ,n_layers, linear_dim, dropout)
+        self.model = LSTMNET(input_size ,n_layers, linear_dim, dropout)
+        self.batch_size = batch_size
         self.criterion = criterion
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
         self.n_epochs = n_epochs
@@ -147,17 +160,24 @@ class LSTMClassifier(Classifier):
         return self.model
 
     def train(self, X_train, y_train):
-        X_train = torch.Tensor(X_train)
-        y_train = torch.LongTensor(y_train)
-        y_tag = None
         for i in range(self.n_epochs):
-            y_pred, y_tag = self.model.forward(X_train,y_tag)  # no batchs ?
-            loss = self.criterion(y_pred, y_train)
-            self.losses.append(loss)
+            self.model.train()
+            X_train, y_train = shuffle(X_train,y_train)
+            X_train_tensor = torch.Tensor(X_train)
+            y_train_tensor = torch.LongTensor(y_train)
+            # y_tag_tensor = None
+            for j in range(int(X_train_tensor.size()[0]//self.batch_size)):
+                
+                y_pred_tensor = self.model.forward(X_train_tensor[j*self.batch_size:(j+1)*self.batch_size])  # no batchs ?
+                loss = self.criterion(y_pred_tensor, y_train_tensor[j*self.batch_size:(j+1)*self.batch_size])
+                self.losses.append(loss)
 
-            self.optimizer.zero_grad()
-            loss.backward()
-            self.optimizer.step()
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+            with torch.no_grad():  
+                y_pred = self.model.forward(X_train_tensor)
+                print("epoch: {}. loss:{}. metrics:{}".format(i,np.mean(self.losses[-1*self.batch_size:]),evaluate_metrics(y_train,y_pred.max(1).indices)))
 
         return None
 
--- driver.py
+++ driver.py
@@ -7,23 +7,23 @@ from classifier import LRClassifier, BasicNN, SVMClassifier, LSTMClassifier
 
 if __name__ == '__main__':
     vectore_size = 50
-    number_of_words=12
+    number_of_words=8
     # vectorize = TFIDF(vectore_size)
     # vectorize = MeanW2V(W2VGlove(),vectore_size)
     # vectorize = MeanW2V(W2VGensim(min_count=1, vector_size=vectore_size, window=5, sg=1),vectore_size)
-    # vectorize = ConcatW2V(W2VGlove(),vectore_size,number_of_words)
-    vectorize = ConcatW2V(W2VReduced('./gensim_reduced.pkl'),vectore_size,number_of_words)
+    vectorize = ConcatW2V(W2VGlove(),vectore_size,number_of_words)
+    # vectorize = ConcatW2V(W2VReduced('./gensim_reduced.pkl'),vectore_size,number_of_words)
     
-    # cls = BasicNN(shape=vectore_size*number_of_words)
+    # cls = BasicNN(input_size=vectore_size*number_of_words)
     # cls = LRClassifier()
     # cls = SVMClassifier(kernel='linear')
     # cls = SVMClassifier(kernel='rbf')
     # cls = SVMClassifier(kernel='poly')
-    cls = LSTMClassifier(vectore_size,12,64,dropout=0.2)
+    cls = LSTMClassifier(vectore_size,3,64,dropout=0.2)
     
     kf = KFoldCV(n_splits=5, shuffle=True)
     ds = preprocess("trump_train.tsv")
     X, y = vectorize.fit_transform(ds['text'], ds['device'])
     # vectorize.w2v.save(ds['text'])
 
-    kf.run_kfold_cv(X, y, cls)
+    kf.run_kfold_cv(X.reshape((-1,number_of_words,vectore_size)), y, cls)
